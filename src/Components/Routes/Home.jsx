b5900b443f0e1dd462c9193a7f33ae1346105a31
mariajimenamacias <mjimenamacias@hotmail.com>
2023-11-25 19:53:33 -0300

    se agregaron 2 imagenes y la descripcion en home

--- src/Components/Routes/Home.jsx
+++ src/Components/Routes/Home.jsx
@@ -13,22 +13,23 @@ const Home = () => {
             <div className="image-section">
                 <div className="image-container first-image-container">
                     <img
-                        src="ruta_de_la_imagen_1"
+                        src="src\assets\img\social-media.png"
                         alt="Imagen 1"
                         className="image"
                     />
                     <p className="image-description">
-                        Descripción de la imagen 1
+                    En Cactus Social Media, entendemos que la clave no es solo crecer, sino florecer. Nuestra experiencia y pasión por la creatividad nos convierten en el socio ideal para hacer que tu marca destaque en el vasto paisaje de las redes sociales.
                     </p>
                 </div>
                 <div className="image-container">
                     <img
-                        src="ruta_de_la_imagen_2"
+                        src="src\assets\img\marketing.png"
                         alt="Imagen 2"
                         className="image"
                     />
                     <p className="image-description">
-                        Descripción de la imagen 2
+                    En Cactus, cultivamos estrategias que florecen. Somos una agencia que se dedicada a transformar marcas en oasis de éxito. Nuestro equipo de expertos en marketing trabaja en sintonía para crear campañas innovadoras y estrategias que destacan en el árido paisaje de la competencia.
+
                     </p>
                 </div>
             </div>
