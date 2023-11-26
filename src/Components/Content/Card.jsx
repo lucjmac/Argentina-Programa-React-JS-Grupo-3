6b2a631d3864a7023a9230dfc6469b451cd6c559
Nicolás Capriz <nicocapriz@gmail.com>
2023-11-26 00:10:20 -0300

    Se modifican estilos a las cards de la pagina de inicio/home

--- src/Components/Content/Card.jsx
+++ src/Components/Content/Card.jsx
@@ -6,22 +6,27 @@ const Card = () => {
     return (
         <>
             <div>
-                <ul className="flex gap-3 justify-center">
+                <ul className="image-section">
                     {
                     // eslint-disable-next-line react/jsx-key
                     contenido.map(contenido => 
                      // eslint-disable-next-line react/jsx-key
-                    <div className="image-section">
-                        <div className="image-container">
+
+                    <div className="row align-items-center image-container  first-image-container">
+                        <div className="col-md-3">
                             <img
                                 src={contenido.imagen}
                                 alt="Imagen 1"
-                                className="image"
+                                className=" image"
                             />
+                        </div>
+                        <div className="col-md-9">
+                            <div className="image-description">
                             <h2>{contenido.titulo}</h2>
-                            <p className="image-description">
+                            <p >
                             {contenido.txt}
                             </p>
+                            </div>
                         </div>
                     </div>
                     )
