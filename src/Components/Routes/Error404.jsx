82684884d34846a3ed4870fbca5cdeca8a7ca38a
mariajimenamacias <mjimenamacias@hotmail.com>
2023-11-26 14:46:24 -0300

    css error 404

--- src/Components/Routes/Error404.jsx
+++ src/Components/Routes/Error404.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import "../../assets/css/error.css"
+
 const Error404 = () => {
     return (
         <>
+        <div className="error">
             <h1>Error 404</h1>
             <h2>Page not found.</h2>
+        </div>
         </>
     );
 };
