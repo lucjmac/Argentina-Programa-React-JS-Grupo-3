e62e3cd6da47c624b1f49a8ee123748bcecd2abe
Nicolás Capriz <nicocapriz@gmail.com>
2023-11-27 17:21:06 -0300

    Se mejora responsive de la page home

--- src/Components/Content/Card.jsx
+++ src/Components/Content/Card.jsx
@@ -4,27 +4,26 @@ import { contenido } from "../../assets/javascript/contenidoCards";
 const Card = () => {
     return (
         <>
-            <div>
+            <div >
                 <ul className="image-section">
                     {contenido.map((contenido, index) => (
                         <div
-                            className={`row align-items-center image-container ${
+                            className={` image-container ${
                                 index === 1 ? "second-image-container" : ""
                             }`}
                         >
-                            <div className="col-md-3">
+                            <div className="col">
                                 <img
                                     src={contenido.imagen}
                                     alt="Imagen 1"
                                     className="image"
                                 />
                             </div>
-                            <div className="col-md-9">
+        
                                 <div className="image-description">
                                     <h2>{contenido.titulo}</h2>
                                     <p>{contenido.txt}</p>
                                 </div>
-                            </div>
                         </div>
                     ))}
                 </ul>
