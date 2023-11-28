4d4c406e9f3714aa24d9eab11c3fc918bfe1b2c0
Lucas Macias <lucasjmacias@gmail.com>
2023-11-28 12:37:49 -0300

    cambios

--- src/Components/Content/Card.jsx
+++ src/Components/Content/Card.jsx
@@ -3,34 +3,33 @@ import { contenido } from "../../assets/javascript/contenidoCards";
 
 const Card = () => {
     return (
-        <>
-            <div >
-                <ul className="image-section">
-                    {contenido.map((contenido, index) => (
-                        <div
-                            className={` image-container ${
-                                index === 1 ? "second-image-container" : ""
-                            }`}
-                        >
-                            <div className="col">
-                                <img
-                                    src={contenido.imagen}
-                                    alt="Imagen 1"
-                                    className="image"
-                                />
-                            </div>
-        
-                                <div className="image-description">
-                                    <h2>{contenido.titulo}</h2>
-                                    <p>{contenido.txt}</p>
-                                </div>
+        <div>
+            <ul className="image-section">
+                {contenido.map((contenido, index) => (
+                    <li
+                        className={`row align-items-center image-container ${
+                            index === 1 ? "second-image-container" : ""
+                        }`}
+                        key={index}
+                    >
+                        <div className="col-md-3">
+                            <img
+                                src={contenido.imagen}
+                                alt="Imagen 1"
+                                className="image"
+                            />
                         </div>
-                    
+                        <div className="col-md-9">
+                            <div className="image-description">
+                                <h4>{contenido.titulo}</h4>
+                                <p>{contenido.txt}</p>
+                            </div>
+                        </div>
+                    </li>
                 ))}
             </ul>
         </div>
-        </>
-    )
+    );
 };
 
 export default Card;
